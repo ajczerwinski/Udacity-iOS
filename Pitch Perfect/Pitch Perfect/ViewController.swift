e50ce0d91f71ba3e3675a5cfabdc4421e9f61857
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-27 14:11:58 -0800

    embed navigation view controller, tie stop button to a new viewcontroller.

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,7 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         stopButton.hidden = true
+        recordButton.enabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +32,7 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(sender: UIButton) {
         
+        recordButton.enabled = false
         recordingInProgress.hidden = false
         stopButton.hidden = false
         print("in recordAudio")
