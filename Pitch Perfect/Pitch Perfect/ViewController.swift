30a57bd4e21e2b289c4ef95d5923f384d03e7d53
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-02-27 13:51:07 -0800

    move code to hide stop button to viewWillAppear function

--- Pitch Perfect/Pitch Perfect/ViewController.swift
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +31,7 @@ class ViewController: UIViewController {
     @IBAction func recordAudio(sender: UIButton) {
         
         recordingInProgress.hidden = false
+        stopButton.hidden = false
         print("in recordAudio")
         
     }
