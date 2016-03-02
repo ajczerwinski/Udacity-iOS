93f1bb73df79027ce3ac0f31805cbe959ff7a67e
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-01 19:54:24 -0800

    add citation for RecordedAudio class where I got help with the initializer section from the Udacity forum

--- Pitch Perfect/Pitch Perfect/RecordedAudio.swift
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -13,6 +13,9 @@ class RecordedAudio: NSObject {
     var filePathUrl: NSURL!
     var title: String!
     
+    // CITATION: I got help figuring out the initializer
+    // portion of the code review from the Udacity forum
+    
     init(filePathUrl: NSURL, title: String) {
         self.filePathUrl = filePathUrl
         self.title = title
