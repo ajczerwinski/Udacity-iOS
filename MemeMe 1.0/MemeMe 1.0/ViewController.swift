a061ba6c01397f9a15b02827573b72857fe426d7
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-09 14:51:59 -0800

    connect outlets from storyboard to viewcontroller, set memetextattributes, sharebutton enable and disable logic

--- MemeMe 1.0/MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/MemeMe 1.0/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBOutlet weak var cameraButtonUI: UIBarButtonItem!
+    
+    @IBOutlet weak var shareButtonUI: UIBarButtonItem!
+    
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var toolbarUI: UIToolbar!
+    
+    // Specify the text attributes to approximate the Impact font
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: NSNumber(double: 3.0)
+    ]
 
     let navBackgroundColor: CGFloat = 235.0 / 255.0
     
@@ -16,6 +37,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.navigationController!.navigationBar.barTintColor = UIColor(red: navBackgroundColor, green: navBackgroundColor, blue: navBackgroundColor, alpha: 0.5)
+        
+        if imagePickerView.image == nil {
+            shareButtonUI.enabled = false
+        } else {
+            shareButtonUI.enabled = true
+        }
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        
     }
 
 
