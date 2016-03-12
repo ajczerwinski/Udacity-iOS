97c477a65397ecc94a6db6a092e9740a2bf77c30
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-12 13:04:21 -0800

    create custom view controller and add 'start over' bar button

---
+++ UIKit Fundamentals II/Make Your Own Adventure Practice/Make Your Own Adventure Practice/MYOAViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Make Your Own Adventure Practice
+//
+//  Created by Allen Czerwinski on 3/12/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class MYOAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .Plain, target: self, action: "startOver")
+        
+    }
+
+
+
+}
+
