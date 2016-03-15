715c24b5b2345355fdf07c681282f3203a0deecb
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-15 12:06:52 -0700

    set up tab view controller and connect to table and collection views, create memecell

---
+++ MemeMe 2.0/MemeMe 2.0/MemeCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCell.swift
+//  MemeMe 2.0
+//
+//  Created by Allen Czerwinski on 3/15/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCell: UICollectionViewCell {
+    
+}
---
+++ MemeMe 2.0/MemeMe 2.0/MemeTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Allen Czerwinski on 3/14/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewControllerController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate /* TO ADD UITableViewDelegate, UITableViewDataSource */ {
+    
+    
+    
+}
