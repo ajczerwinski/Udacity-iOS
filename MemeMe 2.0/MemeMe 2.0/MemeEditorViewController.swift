6c7d34e4f9f1c2690da3ea3130db4257d2919fe7
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-15 17:56:10 -0700

    implement save function, various helper functions, notification center code

--- MemeMe 2.0/MemeMe 2.0/MemeEditorViewController.swift
+++ MemeMe 2.0/MemeMe 2.0/MemeEditorViewController.swift
@@ -11,7 +11,158 @@ import UIKit
 
 class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
+    @IBOutlet weak var imagePickerView: UIImageView!
     
+    @IBOutlet weak var cameraButtonUI: UIBarButtonItem!
+    
+    @IBOutlet weak var shareButtonUI: UIBarButtonItem!
+    
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var toolbarUI: UIToolbar!
+    
+    // Specify the text attributes to approximate the Impact font
+    // CITATION: Found solution to issue I was having where the text color was transparent in Udacity Forums: https://discussions.udacity.com/t/mememe-
+    // transparent-text-in-textfield-no-white-background/21336/2
+    
+    let memeTextAttributes = [
+    
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: NSNumber(double: -3.0)
+    
+    ]
+    
+    // Background Color CGFloat constant for setting navigation bar background
+    
+    let navBackgroundColor: CGFloat = 235.0 / 255.0
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        unsubscribeFromKeyboardNotifications()
+        setTheScreenToDefault()
+        
+    }
+    
+    // Helper function to set screen to default
+    
+    func setTheScreenToDefault() {
+        
+//        navigationController!.navigationBar.barTintColor = UIColor(red: navBackgroundColor, green: navBackgroundColor, blue: navBackgroundColor, alpha: 0.5)
+        
+        cameraButtonUI.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        if imagePickerView.image == nil {
+            shareButtonUI.enabled = false
+        } else {
+            shareButtonUI.enabled = true
+        }
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        topTextField.textAlignment = .Center
+        bottomTextField.textAlignment = .Center
+        
+    }
+    
+    func saveImage() {
+        
+       let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, image: imagePickerView.image!, memedImage: generateMemedImage())
+        
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        navigationController?.navigationBar.hidden = true
+        
+        // Hide toolbar so it doesn't show in the memed image
+        
+        toolbarUI.hidden = true
+        
+        // Render view to an image
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawViewHierarchyInRect(self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        navigationController?.navigationBar.hidden = false
+        
+        // Un-hide toolbar now that memed image is captured
+        
+        toolbarUI.hidden = false
+        
+        return memedImage
+        
+    }
+    
+    
+    
+    // Turn on observers to listen for keyboard
+    // show and hide functions
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide", name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    // Turn off observers
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    // CITATION: changed the conditional in the following two functions
+    // based on feedback from my MemeMe 1.0 review
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if bottomTextField.isFirstResponder() {
+            
+            view.frame.origin.y = getKeyboardHeight(notification) * -1
+            
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
+        if bottomTextField.isFirstResponder() {
+            
+            view.frame.origin.y = 0
+            
+        }
+        
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return keyboardSize.CGRectValue().height
+        
+    }
     
     
     
