8c0edf5c948c04c8855be170928ff026b5eee129
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-16 14:22:28 -0700

    rename tableview controller and create detail view controller in storyboard and swift file

---
+++ MemeMe 2.0/MemeMe 2.0/MemeDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Allen Czerwinski on 3/16/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    
+    
+    
+    
+}
--- MemeMe 2.0/MemeMe 2.0/SentMemesTableViewController.swift
+++ MemeMe 2.0/MemeMe 2.0/SentMemesTableViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class MemeTableViewControllerController: UITableViewController {
+class SentMemesTableViewControllerController: UITableViewController {
     
 //    var memes = [Meme]()
 
