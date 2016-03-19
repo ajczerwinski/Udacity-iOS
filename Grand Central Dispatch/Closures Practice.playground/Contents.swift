94145af2c35a95e402d7e8488891a67d4ce975b3
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-19 14:04:12 -0700

    complete closures playground exercise

--- Grand Central Dispatch/Closures Practice.playground/Contents.swift
+++ Grand Central Dispatch/Closures Practice.playground/Contents.swift
@@ -20,3 +20,73 @@ let closures = [f,
 for fn in closures{
     fn(42)
 }
+
+
+func foo(x:Int) -> Int {
+    return 42 + x
+}
+
+let bar = {(x: Int) -> Int
+    in
+    42 + x
+}
+
+// create a few functions, add them to an array and call them
+
+func curly(n:Int) -> Int {
+    return n * n
+}
+
+func larry(x: Int) -> Int {
+    return x * (x + 1)
+}
+
+func moe(m: Int) -> Int {
+    return m * (m - 1) * (m - 2)
+}
+
+var stooges = [curly, larry, moe]
+
+stooges.append(bar)
+
+for stooge in stooges {
+    stooge(42)
+}
+
+
+
+
+typealias Integer = Int
+
+let z: Integer = 42
+let zz: Int = 42
+
+typealias IntToInt = (Int)->Int
+
+typealias IntMaker = (Void)->Int
+
+func makeCounter() -> IntMaker {
+    var n = 0
+    
+    // Functions within functions?
+    // Yes
+    func adder() -> Int {
+        n = n + 1
+        return n
+    }
+    
+    return adder
+    
+}
+
+let counter1 = makeCounter()
+let counter2 = makeCounter()
+
+counter1()
+counter1()
+counter1()
+
+counter2()
+
+
+
