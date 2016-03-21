a806f6e656a4cb92a60ba5c6ef4985429d3468db
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-20 22:40:46 -0700

    add private api key and nsurl object for url

--- iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/Constants.swift
+++ iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/Constants.swift
@@ -27,7 +27,7 @@ struct Constants {
     
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
-        static let APIKey = "YOUR API KEY HERE"
+        static let APIKey = "74397ca338244a144520cfbfb4c84a01"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
