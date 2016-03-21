890a8b13b910cc51307324e7f66268fbb8a6ec3f
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-20 23:31:56 -0700

    modularize the method parameters and add error handling

--- iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -44,8 +44,83 @@ class ViewController: UIViewController {
         
         // TODO: Write the network code here!
         
-        let url = NSURL(string: "\(Constants.Flickr.APIBaseURL)?\(Constants.FlickrParameterKeys.Method)=\(Constants.FlickrParameterValues.GalleryPhotosMethod)&\(Constants.FlickrParameterKeys.APIKey)=\(Constants.FlickrParameterValues.APIKey)&\(Constants.FlickrParameterKeys.GalleryID)=\(Constants.FlickrParameterValues.GalleryID)&\(Constants.FlickrParameterKeys.Extras)=\(Constants.FlickrParameterValues.MediumURL)&\(Constants.FlickrParameterKeys.Format)=\(Constants.FlickrParameterValues.ResponseFormat)&\(Constants.FlickrParameterKeys.NoJSONCallback)=\(Constants.FlickrParameterValues.DisableJSONCallback)")!
+//        let url = NSURL(string: "\(Constants.Flickr.APIBaseURL)?\(Constants.FlickrParameterKeys.Method)=\(Constants.FlickrParameterValues.GalleryPhotosMethod)&\(Constants.FlickrParameterKeys.APIKey)=\(Constants.FlickrParameterValues.APIKey)&\(Constants.FlickrParameterKeys.GalleryID)=\(Constants.FlickrParameterValues.GalleryID)&\(Constants.FlickrParameterKeys.Extras)=\(Constants.FlickrParameterValues.MediumURL)&\(Constants.FlickrParameterKeys.Format)=\(Constants.FlickrParameterValues.ResponseFormat)&\(Constants.FlickrParameterKeys.NoJSONCallback)=\(Constants.FlickrParameterValues.DisableJSONCallback)")!
         
-        print(url)
+//        print(url)
+
+        
+        
+//        let someParameters = [
+//            "course": "networking",
+//            "nanodegree": "ios",
+//            "quiz": "escaping paramters"
+//        ]
+//        
+//        print(escapedParameters(someParameters))
+    
+        let methodParameters = [
+        
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID: Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            
+        ]
+        
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        // There is a similar method called NSMutableURLRequest(URL: url) that we can use if
+        // we want to change the type of request.HTTPMethod. In this example we can use the regular
+        // NSURLRequest because "GET" is the default type of HTTPMethod, but in the future we'll
+        // use the mutable one when necessary
+        
+        // let request = NSMutableURLRequest(URL: url)
+        //request.HTTPMethod = "GET"
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
+            
+            // if error occurs, print it and re-enable the UI
+            func displayError(error: String) {
+                print(error)
+                print("URL at time of error: \(url)")
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                }
+            }
+            
+            if error == nil {
+                print(data!)
+            }
+        }
+        task.resume()
+    }
+    
+    private func escapedParameters(parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // Make sure that the value is a string
+                let stringValue = "\(value)"
+                
+                // Escape it
+                let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+                
+                // Append it
+                keyValuePairs.append(key + "=" + "\(escapedValue)")
+                
+            }
+            
+            return "?\(keyValuePairs.joinWithSeparator("&"))"
+        }
+        
     }
 }
