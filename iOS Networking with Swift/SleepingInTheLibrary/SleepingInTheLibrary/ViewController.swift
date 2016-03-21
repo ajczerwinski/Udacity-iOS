e1a3eaf3781024aa2a997e9d4353c414199aa9c0
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-21 14:08:02 -0700

    refactor sleeping in the library app to use guard statements to error check

--- iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ iOS Networking with Swift/SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -84,56 +84,114 @@ class ViewController: UIViewController {
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) in
             
 //            // if error occurs, print it and re-enable the UI
-//            func displayError(error: String) {
-//                print(error)
-//                print("URL at time of error: \(url)")
-//                performUIUpdatesOnMain {
-//                    self.setUIEnabled(true)
-//                }
-//            }
-            
-            if error == nil {
-                // there was data returned
-                if let data = data {
-                    
-                    let parsedResult: AnyObject!
-                    do {
-                        parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
-                    }
-                    catch {
-//                        displayError("Could not parse the data as JSON: '\(data)'")
-                        return
-                    }
-                    print(parsedResult)
-                    
-                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject],
-                        photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] {
-//                            print(photoArray[0])
-                            
-                            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
-                            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
-                            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
-                            
-                            if let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String {
-                                let imageURL = NSURL(string: imageUrlString)
-                                if let imageData = NSData(contentsOfURL: imageURL!) {
-                                    performUIUpdatesOnMain {
-                                        self.setUIEnabled(true)
-                                        self.photoImageView.image = UIImage(data: imageData)
-                                        self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
-                                    }
-                                }
-                            }
-                            
-                        }
-                    
-                    }
-                
+            func displayError(error: String) {
+                print(error)
+                print("URL at time of error: \(url)")
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
                 }
-                
+            }
+            
+            // GUARD: Was there an error?
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(error)")
+                return
+            }
+            
+            // GUARD: Did we get a successful 2XX response?
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode
+                where statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2xx!")
+                    return
+            }
+            
+            // GUARD: Was there any data returned?
+            guard let data = data else {
+                displayError("No data was returned by the request!")
+                return
+            }
+            
+            // parse the data
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            }
+            catch {
+                displayError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            // GUARD: Did Flickr return an error (stat != ok)?
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String where stat == Constants.FlickrResponseValues.OKStatus else {
+                displayError("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            // GUARD: Are the "photos" and "photo" keys in our result?
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject], photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] else {
+                displayError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' and '\(Constants.FlickrResponseKeys.Photo)' in \(parsedResult)")
+                return
+            }
+            
+            // select a random photo
+            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
+            
+            // GUARD: Does our photo have a key for 'url_m'?
+            guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoDictionary)")
+                return
+            }
+            
+            // If an image exists at the url, set the image and title
+            let imageURL = NSURL(string: imageUrlString)
+            if let imageData = NSData(contentsOfURL: imageURL!) {
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                    self.photoImageView.image = UIImage(data: imageData)
+                    self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+                    }
+                } else {
+                    displayError("Image does not exist at \(imageURL)")
+                }
             }
         task.resume()
     }
+    
+//            if error == nil {
+                // there was data returned
+//                if let data = data {
+                
+                    
+//                    print(parsedResult)
+                    
+//                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject],
+//                        photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:AnyObject]] {
+//                            print(photoArray[0])
+                            
+//                            let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+//                            let photoDictionary = photoArray[randomPhotoIndex] as [String:AnyObject]
+//                            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
+                            
+//                            if let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String {
+//                                let imageURL = NSURL(string: imageUrlString)
+//                                if let imageData = NSData(contentsOfURL: imageURL!) {
+//                                    performUIUpdatesOnMain {
+//                                        self.setUIEnabled(true)
+//                                        self.photoImageView.image = UIImage(data: imageData)
+//                                        self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+//                                    }
+//                                }
+//                            }
+            
+//                        }
+            
+//                    }
+                
+//                }
+            
+//            }
     
     private func escapedParameters(parameters: [String:AnyObject]) -> String {
         
