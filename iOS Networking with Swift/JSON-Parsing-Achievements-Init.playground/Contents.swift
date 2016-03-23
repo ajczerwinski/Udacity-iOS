356a27605f9f3064922514a7ac7c61280e394459
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-22 17:40:01 -0700

    complete starcraft achievements exercise

--- iOS Networking with Swift/JSON-Parsing-Achievements-Init.playground/Contents.swift
+++ iOS Networking with Swift/JSON-Parsing-Achievements-Init.playground/Contents.swift
@@ -23,47 +23,105 @@ var parsedAchievementsJSON = try! NSJSONSerialization.JSONObjectWithData(rawAchi
 func parseJSONAsDictionary(dictionary: NSDictionary) {
     /* Start playing with JSON here... */
     
+    // Set initial variables to store the achievement count and sum
     var numAchievements = 0
     var sumAchievements: Double = 0.0
     
+    // Create array to hold the categoryIds for "Matchmaking" categories
+    var matchMakingIds: [Int] = []
+    
+    // Create dictionary to store the counts for "Matchmaking" categories
+    var categoryCounts: [Int: Int] = [:]
+    
+    
+    // Get top level categories
     guard let arrayOfCategoryDictionaries = parsedAchievementsJSON["categories"] as? [[String:AnyObject]] else {
         print("Cannot find key 'categories' in \(parsedAchievementsJSON)")
         return
     }
     
+    // Store all "Matchmaking" categories
     for categoryDictionary in arrayOfCategoryDictionaries {
-        guard let matchmakingCategory = categoryDictionary["matchmaking"] as? String else {
-            print("not working")
-            return
+        
+        if let title = categoryDictionary["title"] as? String where title == "Matchmaking" {
+            
+            guard let children = categoryDictionary["children"] as? [NSDictionary] else {
+                print("Cannot find key 'children' in \(categoryDictionary)")
+                return
+            }
+            
+            for child in children {
+                
+                guard let categoryId = child["categoryId"] as? Int else {
+                    print("Cannot find key 'categoryId' in \(child)")
+                    return
+                }
+                
+                matchMakingIds.append(categoryId)
+            }
         }
     }
     
+    // Get top level achievements
     guard let arrayOfAchievmentsDictionaries = parsedAchievementsJSON["achievements"] as? [[String:AnyObject]] else {
         print("Cannot find key 'achievements' in \(parsedAchievementsJSON)")
         return
     }
     
-    print(parsedAchievementsJSON)
+//    print(parsedAchievementsJSON)
+    
     for achievementDictionary in arrayOfAchievmentsDictionaries {
+        
+        // Find achievement points in the data
         guard let achievementPoints = achievementDictionary["points"] as? Int else {
             print("Cannot find key 'points' in \(achievementDictionary)")
             return
         }
         
+        // Count total number of achievements and sum of total points
         numAchievements++
         sumAchievements += Double(achievementPoints)
-//        print(sumAchievements / Double(numAchievements))
         
-        
+        // Count the number of achievements with a point value of more than 10
         if achievementPoints > 10 {
             print("We got one!")
         }
         
+        // Access the "Cool Running" achievement's description and see how to achieve it in the game
         if let titleText = achievementDictionary["title"] as? String where titleText.rangeOfString("Cool Running") != nil {
             print("Here is what you must do to achieve the 'Cool Running' achievement: \(achievementDictionary["description"]!)")
         }
         
+        // Add to category counts
+        guard let categoryId = achievementDictionary["categoryId"] as? Int else {
+            print("Cannot find key 'categoryId' in \(achievementDictionary)")
+            return
+        }
+        
+        // If the category doesn't have a key in the dictionary, set it to 0
+        if categoryCounts[categoryId] == nil {
+            categoryCounts[categoryId] = 0
+        }
+        
+        // Count the number of matchmaking a
+        if let currentCount = categoryCounts[categoryId] {
+            categoryCounts[categoryId] = currentCount + 1
+        }
+        
+        
     }
+    
+    var matchmakingAchievementCount = 0
+    
+    // Count the number of "Matchmaking" achievements
+    for matchmakingId in matchMakingIds {
+        if let categoryCount = categoryCounts[matchmakingId] {
+            matchmakingAchievementCount += categoryCount
+        }
+    }
+    
+    // Print the result
+    print("\(matchmakingAchievementCount) achievements belong to the \"Matchmaking\" category")
     
     let averageOfAchievements = sumAchievements / Double(numAchievements)
     print(averageOfAchievements)
