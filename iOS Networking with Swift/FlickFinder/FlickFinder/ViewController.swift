1f86bbd7c6c1553dd9578f359b94ed702d78de38
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-23 16:34:45 -0700

    complete implementation of Flick Picker

--- iOS Networking with Swift/FlickFinder/FlickFinder/ViewController.swift
+++ iOS Networking with Swift/FlickFinder/FlickFinder/ViewController.swift
@@ -285,11 +285,92 @@ class ViewController: UIViewController {
         // create network request
         let task = session.dataTaskWithRequest(request) { (data, response, error) in
          
+            // if an error occurs, print it and re-enable the UI
+            func displayError(error: String) {
+                print(error)
+                performUIUpdatesOnMain {
+                self.setUIEnabled(true)
+                self.photoTitleLabel.text = "No photo returned. Try again."
+                self.photoImageView.image = nil
+                }
+            }
             
+            // Was there an error?
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(error)")
+                return
+            }
+            
+            // Did we get a successful 2XX response?
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            // Was there any data returned?
+            guard let data = data else {
+                displayError("No data was returned by the request!")
+                return
+            }
+            
+            // Parse the data
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                displayError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            // Did Flickr return an error (stat != ok)?
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String where stat == Constants.FlickrResponseValues.OKStatus else {
+                displayError("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            // Is the "photo" key in our result?
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject] else {
+                displayError("Cannot find key \(Constants.FlickrResponseKeys.Photos)' in \(parsedResult)")
+                return
+            }
+            
+            // Is the "photo" key in photosDictionary?
+            guard let photosArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] else {
+                displayError("Cannot find key '\(Constants.FlickrResponseKeys.Photo)' in \(photosDictionary)")
+                return
+            }
+            
+            if photosArray.count == 0 {
+                displayError("No Photos Found. Search Again.")
+                return
+            } else {
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photosArray.count)))
+                let photoDictionary = photosArray[randomPhotoIndex] as [String: AnyObject]
+                let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String
+                
+                // Does our photo have a key for 'url_m'?
+                guard let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                    displayError("Cannot find key '\(Constants.FlickrResponseKeys.MediumURL)' in \(photoDictionary)")
+                    return
+                }
+                
+                // if an image exists at the url, set the image and title
+                let imageURL = NSURL(string: imageUrlString)
+                if let imageData = NSData(contentsOfURL: imageURL!) {
+                    performUIUpdatesOnMain {
+                        self.setUIEnabled(true)
+                        self.photoImageView.image = UIImage(data: imageData)
+                        self.photoTitleLabel.text = photoTitle ?? "(Untitled)"
+                    }
+                } else {
+                    displayError("Image does not exist at \(imageURL)")
+                }
+            }
             
         }
         
-        
+        // Start the task!
+        task.resume()
         
     }
     
