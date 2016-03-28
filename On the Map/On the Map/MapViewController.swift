c5cc72ffe1cf9b0cfe32610889a7dc51cc80777e
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-28 16:05:47 -0700

    add mapview controller and ui with dummy data to show pins

---
+++ On the Map/On the Map/MapViewController.swift
@@ -0,0 +1,123 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by Allen Czerwinski on 3/28/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        
+        // Load hard coded locations for the time being
+        // TODO: change to dynamically loaded locations from StudentLocation struct
+        let locations = hardCodedLocationData()
+        
+        var annotations = [MKPointAnnotation]()
+        
+        // TODO: iterate through the dictionary to be created in the locations object
+        for dictionary in locations {
+            
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let first = dictionary["firstName"] as! String
+            let last = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(first) \(last)"
+            annotation.subtitle = mediaURL
+            
+            annotations.append(annotation)
+            
+        }
+        
+        self.mapView.addAnnotations(annotations)
+        
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+        
+    }
+    
+    func mapView(mapVie: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(NSURL(string: toOpen)!)
+            }
+        }
+        
+    }
+    
+    
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        
+        return [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Bob",
+                "lastName" : "Smith",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+                
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Jed",
+                "lastName" : "Jones",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ],
+               [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Ted",
+                "lastName" : "Toomey",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 2362758535,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ]
+        ]
+    }
+    
+    
+}
