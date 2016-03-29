c9d8776b1a3f1bb7970f5f44b13d18b89bdb7419
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-03-28 20:00:09 -0700

    add MVC architecture and UdacityConstants

---
+++ On the Map/On the Map/UdacityConstants.swift
@@ -0,0 +1,20 @@
+//
+//  UdacityConstants.swift
+//  On the Map
+//
+//  Created by Allen Czerwinski on 3/28/16.
+//  Copyright © 2016 Allen Czerwinski. All rights reserved.
+//
+
+import UIKit
+
+struct UdacityConstants {
+    
+    struct Udacity {
+        static let ApiScheme = "https"
+        static let ApiHost = "udacity.com"
+        static let ApiPath = "/api"
+    }
+}
+
+
