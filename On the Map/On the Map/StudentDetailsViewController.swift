6ee998afa0a9123618427a2e6797ab346ae9aa9d
Allen Czerwinski <allen.czerwinski@gmail.com>
2016-04-08 18:17:10 -0700

    bug fix UI layouts

--- On the Map/On the Map/StudentDetailsViewController.swift
+++ On the Map/On the Map/StudentDetailsViewController.swift
@@ -28,7 +28,65 @@ class StudentDetailsViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        findOnMapButtonUI.layer.cornerRadius = 6
+        submitButtonUI.layer.cornerRadius = 6
+        
+        enterURL.delegate = self
+        enterLocation.delegate = self
+        
+        presentFirstUI()
+        
+    }
     
+    
+    func presentFirstUI() {
+        
+        let cancelButtonColorRed: CGFloat = 57.0 / 255.0
+        let cancelButtonColorGreen: CGFloat = 96.0 / 255.0
+        let cancelButtonColorBlue: CGFloat = 139.0 / 255.0
+        cancelButtonUI.tintColor = UIColor(red: cancelButtonColorRed, green: cancelButtonColorGreen, blue: cancelButtonColorBlue, alpha: 1.0)
+        
+        enterURL.hidden = true
+        mapView.hidden = true
+        submitButtonUI.hidden = true
+        
+        enterLocation.hidden = false
+        
+        whereUStudyingTop.hidden = false
+        whereUStudyingMiddle.hidden = false
+        whereUStudyingBottom.hidden = false
+        
+        topUIBar.hidden = false
+        bottomUIBar.hidden = false
+        
+        findOnMapButtonUI.hidden = false
+
+        
+    }
+    
+    func presentSecondUI() {
+        
+        cancelButtonUI.tintColor = UIColor.whiteColor()
+        
+        enterURL.hidden = false
+        mapView.hidden = false
+        submitButtonUI.hidden = false
+        
+        enterLocation.hidden = true
+        
+        whereUStudyingTop.hidden = true
+        whereUStudyingMiddle.hidden = true
+        whereUStudyingBottom.hidden = true
+        
+        topUIBar.hidden = true
+        bottomUIBar.hidden = true
+        
+        findOnMapButtonUI.hidden = true
+        
+    }
     
     @IBAction func cancelButtonPressed(sender: AnyObject) {
         
